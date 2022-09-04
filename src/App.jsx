70f4b3a8b68c4fccf6827faf2c590557c998b8ec
enjeytee <enjeytee@gmail.com>
2022-09-04 20:34:14 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -177,3 +177,50 @@
 // };
 
 // END OF FOURTH LESSON - CONSTRUCTOR METHOD CHALLENGE //
+
+
+// FIFTH LESSON - UPDATING COMPLEX STATE WITH this.setState() //
+
+import React from "react"
+
+export default function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+    
+    let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
+    
+    function toggleFavorite() {
+        setContact(prevContact => ({
+            ...prevContact,
+            isFavorite: !prevContact.isFavorite
+        }))
+    }
+    
+    return (
+        <main>
+            <article className="card">
+                <img src="./images/user.png" className="card--image" />
+                <div className="card--info">
+                    <img 
+                        src={`../images/${starIcon}`} 
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                    />
+                    <h2 className="card--name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="card--contact">{contact.phone}</p>
+                    <p className="card--contact">{contact.email}</p>
+                </div>
+                
+            </article>
+        </main>
+    )
+}
+
+// FIFTH LESSON - UPDATING COMPLEX STATE WITH this.setState() //
